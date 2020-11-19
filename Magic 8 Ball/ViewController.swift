ee1dc53c816d4219a8ab227d5b0bc675ea337f4f
Samieh Jarabba <abdels6375@gmail.com>
2020-11-18 19:00:29 -0600

    Update and Push

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,13 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    @IBOutlet weak var imageView: UIImageView!
+        
+    @IBOutlet func askButtonPressed(_ sender: UIButton) {
+        
+    }
+        
+    
+    
 }
 
